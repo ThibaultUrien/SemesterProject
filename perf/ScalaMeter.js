344c207ae6f763f555a99c1139222a886907b281
Thibault Urien <thibault.urien@gmail.com>
2016-06-08 16:58:42 +0200

    clean repo

--- perf/ScalaMeter.js
+++
@@ -1,6 +0,0 @@
-var ScalaMeter = (function(parent) {
-  var my = { name: "data" };
-  my.index = [{"scope" : ["dotty", "stdlib"], "name" : "stdlib", "file" : "..\/dotty.stdlib.stdlib.dsv"}, {"scope" : ["dotty", "dotty-src"], "name" : "dotty-src", "file" : "..\/dotty.dotty-src.dotty-src.dsv"}];
-  parent[my.name] = my;
-  return parent;
-})(ScalaMeter || {});
